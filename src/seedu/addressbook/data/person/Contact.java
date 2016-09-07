10f01f271a76b332867c235c0d1ec223a1363d3e
zuo zhuolin <zhuolinzuo@gmail.com>
2016-09-07 14:41:11 +0800

    Use Inheritance to achieve code reuse

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+    private boolean isPrivate;
+    
+    public Contact(boolean isPrivate){
+        this.isPrivate = isPrivate;
+    }
+    
+    public boolean isPrivate(){
+        return isPrivate;
+    }
+}
--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email extends Contact {
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
@@ -14,15 +14,14 @@ public class Email {
     public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
 
     public final String value;
-    private boolean isPrivate;
-
+   
     /**
      * Validates given email.
      *
      * @throws IllegalValueException if given email address string is invalid.
      */
     public Email(String email, boolean isPrivate) throws IllegalValueException {
-        this.isPrivate = isPrivate;
+        super(isPrivate);
         email = email.trim();
         if (!isValidEmail(email)) {
             throw new IllegalValueException(MESSAGE_EMAIL_CONSTRAINTS);
@@ -55,7 +54,5 @@ public class Email {
     }
 
 
-    public boolean isPrivate() {
-        return isPrivate;
-    }
+   
 }
